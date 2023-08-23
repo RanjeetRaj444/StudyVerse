47bd7106c80f8f13f05c2d9da220a9c4dae717f4
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-08-23 21:36:02 +0530

    day-2

---
+++ fontend/src/Parmeshwar/ExpereSolution.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ExpereSolution = () => {
+  return (
+    <div>
+        <img src="https://quizlet.com/_next/static/media/landing-page-header-jagged-lines-day.ea330eb6.svg" alt="" />
+    </div>
+  )
+}
