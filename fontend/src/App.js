c3aa2b3140e3dc004f0560598efb0aeb522cc506
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-08-25 22:07:38 +0530

    up

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,15 +1,23 @@
+
+
 import logo from './logo.svg';
 import './App.css';
-
-
+import { ExpereSolution } from './Product/ExpereSolution';
+import SignIn from './Pages/SignInPage';
+import SignUp from './Pages/SignUpPage';
+// import Navbar from "./Admin/Pages/Navbar";
+// import MainPageAdimn from "./Pages/MainPageAdimn";
+// import MainRoutes from "./Routes/Routes";
+// import Routes from './Routes/Routes';
 
 
 function App() {
   return (
     <div>
-     
+    <ExpereSolution/>
     </div>
   );
-}
+
+};
 
 export default App;
