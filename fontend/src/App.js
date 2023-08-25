c6a8826c6858707c41676055b5d890d92a8cc65e
ranjeetraj444 <ranjeetraj44666@gmail.com>
2023-08-25 22:58:17 +0530

    ....

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { ExpereSolution } from './Parmeshwar/ExpereSolution';
-import SignIn from './Pages/SignInPage';
-import SignUp from './Pages/SignUpPage';
+import logo from "./logo.svg";
+import "./App.css";
+// import { ExpereSolution } from "./Parmeshwar/ExpereSolution";
+import SignIn from "./Pages/SignInPage";
+import SignUp from "./Pages/SignUpPage";
 // import Navbar from "./Admin/Pages/Navbar";
 // import MainPageAdimn from "./Pages/MainPageAdimn";
 // import MainRoutes from "./Routes/Routes";
 // import Routes from './Routes/Routes';
 
-
 function App() {
-  return (
-    <div>
-    <ExpereSolution/>
-    </div>
-  );
-
-};
+	return (
+		<div>
+			{/* <ExpereSolution /> */}
+		</div>
+	);
+}
 
 export default App;
