800c70ff0ef659e353dd96bedc4fc1135a43cd05
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-08-25 21:54:12 +0530

    DD

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,18 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-// import { ExpereSolution } from './Product/ExpereSolution';
-// import { SingleProduct } from './Product/SingleProduct';
+
 
 
 
 function App() {
   return (
     <div>
-      {/* <ExpereSolution /> */}
-      {/* <SingleProduct /> */}
-    
-
-    {/* <Try /> */}
+     
     </div>
   );
 }
