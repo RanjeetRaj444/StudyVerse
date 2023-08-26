291c2de1f083ee4ee6f704d9f7d452bea08a2917
ranjeetraj444 <ranjeetraj44666@gmail.com>
2023-08-26 22:00:14 +0530

    Day5 work done

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { connection } = require("./connection/db");
 
-const userRouter=require("./routes/user.routes");
+const userRouter = require("./routes/user.routes");
 const adminRouter = require("./routes/admin.routes");
 require("dotenv").config();
 const cors = require("cors");
@@ -11,21 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("Welcome to home page")
+app.get("/", (req, res) => {
+	res.send("Welcome to home page");
 });
 
-app.use("/admin",adminRouter)
-app.use("/user",userRouter)
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
 app.use("/books", bookRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send("Invalid route")
-    next();
+	res.status(404).send("Invalid route");
+	next();
 });
-
-
 
 app.listen(process.env.PORT, async () => {
 	try {
--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,9 +1,10 @@
-
 import logo from "./logo.svg";
 import "./App.css";
 // import { ExpereSolution } from "./Parmeshwar/ExpereSolution";
 import SignIn from "./Pages/SignInPage";
 import SignUp from "./Pages/SignUpPage";
+import MainRoutes from "./Routes/Routes";
+import UserLogo from "./Components/UserLogo";
 // import Navbar from "./Admin/Pages/Navbar";
 // import MainPageAdimn from "./Pages/MainPageAdimn";
 // import MainRoutes from "./Routes/Routes";
@@ -13,6 +14,8 @@ function App() {
   return (
     <div>
     {/* <ExpereSolution/> */}
+    <MainRoutes/>
+    <UserLogo/>
     </div>
   );
 
